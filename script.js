d647385124ab0ff21a12357ee4ce257e647b4aaf
RECA India <aakashm301@gmail.com>
2020-08-26 10:04:52 +0530

    Update script.js
    Bug fixes.

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ function complete() {
 async function getQuote() {
   loading();
   // We need to use a Proxy URL to make our API call in order to avoid a CORS error
-  const proxyUrl = 'http://cors-anywhere.herokuapp.com/';
-  const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+  const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
   try {
     const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
